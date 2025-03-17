e601b691ab737f4cb7adf98fef53465faf8987b3
Tigaas <trr08469@gmail.com>
2025-03-17 15:43:21 -0300

    CalcIMC_1703

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'], // Corrigido "styleUrls"
   standalone: false,
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Calc_IMC';
+
+  peso: number = 0;
+  altura: number = 0;
+  imc: number | null = null; // Pode ser nulo antes do cálculo
+  mensagem: string = '';
+
+  calcularIMC() {
+    if (this.altura <= 0) {
+      this.mensagem = "Erro: A altura deve ser maior que zero!";
+      this.imc = null;
+      return;
+    }
+
+    this.imc = parseFloat((this.peso / (this.altura * this.altura)).toFixed(2));
+    this.definirMensagem();
+  }
+
+  definirMensagem() {
+    if (this.imc !== null) {
+      if (this.imc < 18.5) {
+        this.mensagem = 'Abaixo do peso';
+      } else if (this.imc >= 18.5 && this.imc < 24.9) {
+        this.mensagem = 'Peso normal';
+      } else if (this.imc >= 25 && this.imc < 29.9) {
+        this.mensagem = 'Sobrepeso';
+      } else {
+        this.mensagem = 'Obesidade';
+      }
+    }
+  }
 }
